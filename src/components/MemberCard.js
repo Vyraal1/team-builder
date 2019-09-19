a80938c07e1caf9a385a312193c943ffa2dea53e
Vyraal1 <kevin.afable@gmail.com>
2019-09-19 16:50:10 -0400

    adding the components

---
+++ src/components/MemberCard.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function TeammateCard() {
+  return <div>TeammateCard</div>;
+}
+
+export default TeammateCard;
---
+++ src/components/TeamList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MemberCard from "./MemberCard";
+
+function TeamList() {
+  return (
+    <div>
+      <MemberCard /> <MemberCard /> <MemberCard />
+    </div>
+  );
+}
+
+export default TeamList;
